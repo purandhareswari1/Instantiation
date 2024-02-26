aba600262bf1f03fc22da6742bd9478a0a12de3d
purandhareswari1 <purandhareswarikanduri@gmai.com>
2024-02-26 13:18:01 +0530

    Commit

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,3 @@
-
 public class Lab {
     /**
      * Java relies very heavily on OOP, so all of our code will rely on Objects. Before we write our own objects,
@@ -18,6 +17,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+    Object instance = new Object();
+            return instance;
+        }
     }
-}
